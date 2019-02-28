a8d9dd8efbfcc5f4990ec4d929a261fa382e1fba
Mike Aarset <michael.aarset@gmail.com>
2019-02-28 14:00:22 -0800

    Adding everything

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ComplaintsComponent } from './complaints/complaints.component';
+import { CommentsComponent } from './comments/comments.component';
+import { ImdbComponent } from './imdb/imdb.component';
 
-const routes: Routes = [];
+import { County } from './Models/county';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'comments',
+    component: CommentsComponent
+  },
+  {
+    path: 'complaints',
+    component: ComplaintsComponent
+  },
+  {
+    path: 'imdb',
+    component: ImdbComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NgxPaginationModule} from 'ngx-pagination';
 
 import { AppComponent } from './app.component';
-
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { CommentsComponent } from './comments/comments.component';
+import { ImdbComponent } from './imdb/imdb.component';
+import { ComplaintsComponent } from './complaints/complaints.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CountyPdComponent } from './county-pd/county-pd.component';
+import { ComplaintFormComponent } from './complaints/complaint-form/complaint-form.component';
+import { ComplaintEditComponent } from './complaints/complaint-edit/complaint-edit.component';
+//import {MatDatepickerModule} from '@angular/material/datepicker';
+//npm install --save @angular/material @angular/cdk
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+    CommentsComponent,
+    ImdbComponent,
+    ComplaintsComponent,
+    CountyPdComponent,
+    ComplaintFormComponent,
+    ComplaintEditComponent
+    //MatDatepickerModule 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    HttpModule,
+    NgxPaginationModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
